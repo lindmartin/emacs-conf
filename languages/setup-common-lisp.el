a53ca4fd0b5ecf2209b9807a24914b73b76f0641
Martin Lind <martin@lindsystem.se>
2014-12-26 18:18:26 +0100

    Enable use of quicklisp helper.

--- languages/setup-common-lisp.el
+++ languages/setup-common-lisp.el
@@ -1,8 +1,12 @@
 ;;; Setup SLIME with Steel Bank Common Lisp
-(add-to-list 'load-path "~/Tools/slime")
-(require 'slime)
+(if use-quicklisp-slime
+    (load (expand-file-name (concat quicklisp-root-dir
+				    "/slime-helper.el")))
+  (progn
+    (add-to-list 'load-path slime-root-dir)
+    (require 'slime)))
 
-(setq inferior-lisp-program "/usr/local/bin/sbcl"
+(setq inferior-lisp-program "sbcl"
       lisp-indent-function 'common-lisp-indent-function
       slime-complete-symbol-function 'slime-fuzzy-complete-symbol
       slime-startup-animation nil)
